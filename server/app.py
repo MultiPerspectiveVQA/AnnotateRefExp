e712f1f947282cb2ad9ba89c99f5aaed05ec30a7
anushkumarv <anush070@gmail.com>
2024-01-05 15:43:25 +0530

    new route for backend

--- server/app.py
+++ server/app.py
@@ -1,7 +1,10 @@
+import base64
 from bson import ObjectId
-from flask import Flask, jsonify, request, redirect, url_for
-from mongo_util import get_annotation_data, get_annotated_data, submit_annotated_datapoint
+from constants import IMAGE_FILES
+from flask import Flask, jsonify, request, redirect, url_for, send_file
+from mongo_util import get_annotation_data, get_annotated_data, write_annotated_datapoint
 import random
+import os
 
 app = Flask(__name__)
 
@@ -15,29 +18,34 @@ def hello_world():
 @app.route("/next")
 def annotate():
     if len(annotation_data) != 0:
-        message = "Fetching next datapoint"
+        is_present = True
         keys = list(annotation_data.keys())
         annotation_datapoint = annotation_data.get(random.choice(keys))
     else:
-        message =  "Hooray! We have annotated all available data"
+        is_present = False
         annotation_datapoint = {}
-    resp = {"message": message, "annotation_data": annotation_datapoint}
+    resp = {"is_present": is_present, "annotation_data": annotation_datapoint}
     return jsonify(resp), 200
 
 @app.route("/submit", methods=["POST"])
 def submit_annotated_datapoint():
-    if request.type != "POST" and request.mimetype != 'application/json':
-        return jsonify({"message": "Invalid request type", "annotation_data": {}}), 200
-    req_payload = request.json
-    annotated_datapoint = req_payload.get("annotated_datapoint")
+    # if request.type != "POST" and request.mimetype != 'application/json':
+    #     return jsonify({"message": "Invalid request type", "annotation_data": {}}), 200
+    annotated_datapoint = request.json
+    print(request.json)
     annotated_datapoint["_id"] = ObjectId(annotated_datapoint["_id"])
-    submit_annotated_datapoint(annotated_datapoint)
-    annotation_data.pop(annotated_datapoint.get("_id"))
+    write_annotated_datapoint(annotated_datapoint)
+    annotation_data.pop(str(annotated_datapoint.get("_id")))
     return redirect(url_for("annotate"))
 
 @app.route("/list-annotated")
 def list_annotated():
     annotated_data = get_annotated_data()
     message = f"We have annotated {len(annotated_data)} datapoints"
-    resp = {"message": message, "annotated_data": annotated_data}
+    resp = {"is_present": True if len(annotation_data) > 0 else False, "annotated_data": annotated_data}
     return jsonify(resp), 200
+
+@app.route("/get-image/<imagefile>", methods=["GET"])
+def get_image(imagefile):
+    file_path = os.path.join(IMAGE_FILES, imagefile) if imagefile != "logo.jpg" else "logo.jpg"
+    return send_file(file_path, mimetype='image/jpg')
