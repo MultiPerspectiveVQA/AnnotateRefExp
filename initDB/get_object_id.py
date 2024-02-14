abfd1927f5440d7f3db339bf1b5c6308565f9c87
anushkumarv <anush070@gmail.com>
2024-02-14 12:05:47 -0700

    helper script for retriving object id

--- initDB/get_object_id.py
+++ initDB/get_object_id.py
@@ -2,11 +2,12 @@ import pymongo
 from bson.objectid import ObjectId
 
 db_name = "vqa-therapy"
-col_name = "annotated-val"
+col_name = "annotated-train"
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client[db_name]
 col = db[col_name]
 
 
-doc = col.find_one({"question" : "What is it?","answers":["blister","foot"]})
+doc = col.find_one({"question": "What is the color of the flower vase?", "answers":["blue and white", "blue white", "blue"]})
+
 print(doc)
