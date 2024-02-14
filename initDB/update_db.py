58f2c091e0bd47e7f5ed46f26bbcea29a6b35f69
anushkumarv <anush070@gmail.com>
2024-02-14 12:07:18 -0700

    updating datapoint in DB

--- initDB/update_db.py
+++ initDB/update_db.py
@@ -9,7 +9,7 @@ col = db[col_name]
 
 
 
-doc = col.find_one_and_update({"_id" : ObjectId("658e60e4619ad78835d3da44")},{"$set":
-    {"object_lookup": "it", "ref exp": "it",'ambiguous_question': 'Yes'}
+doc = col.find_one_and_update({"_id" : ObjectId("658e61f5f5455c13a828c2df")},{"$set":
+    {"ambiguous_question": "Yes"}
 },upsert=True
 )
